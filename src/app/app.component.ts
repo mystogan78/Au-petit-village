389e5931f442b0b05a064a667aef4f8632e82836
Mystogan78 <ibouba782@gmail.com>
2025-03-15 14:28:36 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component, importProvidersFrom } from '@angular/core';
+import { Component, HostListener, PLATFORM_ID, Inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -8,10 +10,31 @@ import { MatCardModule } from '@angular/material/card';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, MatGridListModule, MatCardModule],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, MatGridListModule, MatCardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Au-petit-village';
+  gridCols: number = 3;
+  cards = [
+    { title: 'Carte 1' },
+    { title: 'Carte 2' },
+    { title: 'Carte 3' },
+    { title: 'Carte 4' }
+  ];
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.onResize(); // ✅ Appeler seulement si c'est le navigateur
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event?: Event) {
+    if (isPlatformBrowser(this.platformId)) { // ✅ Vérifier si on est bien côté client
+      const width = window.innerWidth;
+      this.gridCols = width < 600 ? 1 : width < 900 ? 2 : 3;
+    }
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
-import { NgClass } from '@angular/common';
-import { RouterLink, RouterLinkActive } from '@angular/router';
-
+import { RouterLinkActive } from '@angular/router';
+import { RouterLink} from '@angular/router';
 
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NgClass, RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
   title = "Au Petit Village";
-  
   logo = "assets/images/au-petit-village.png";
-  isFlex = true; // Active ou désactive le display flex
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
 }
